9f7a9e7d85473b0e8633ebf8d4e39df2b36b6a41
kyfex-uwu <retrogrape117@gmail.com>
2024-01-13 14:01:49 -0700

    more rendering
    -changed color mode prompt to be more succinct
    -added positionToShow to ChessRenderer, so you can now also see the piece for which the moves are being shown
    -removed unnecessary lines when drawing in color
    -removed getOpposite, unneeded
    -added a lot more error messages if a command fails, so you know why (untested)

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -9,16 +9,17 @@ import static chess.ChessRenderer.renderGame;
 
 public class Main {
     private static Collection<ChessMove> movesToShow = List.of();
+    private static ChessPosition positionToShow = null;
     public static void main(String[] args) {
         var game = new ChessGame();
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Color mode? (y)es/(n)o\n" +
-                "(if you're not sure, if \u001b[91m this \u001b[0m is red, then you can enable color mode) ");
+                "(if you're not sure, enable color mode if \u001b[91m this \u001b[0m is red) ");
         String ans = scanner.nextLine();
         if(ans.startsWith("y")) ChessRenderer.canShowColor=true;
         while (true) {
-            renderGame(game, ChessRenderer.RenderData.from(movesToShow));
+            renderGame(game, ChessRenderer.RenderData.from(movesToShow, positionToShow));
             System.out.println();
             String line = scanner.nextLine();
 
@@ -28,22 +29,38 @@ public class Main {
                         "move [position from] [position to]: moves a piece from position to position");
             }else if(line.startsWith("show")){
                 try {
-                    movesToShow = game.validMoves(new ChessPosition(line.charAt(6) - 48, line.charAt(5) - 96));
+                    var start=new ChessPosition(line.charAt(6) - 48, line.charAt(5) - 96);
+                    movesToShow = game.validMoves(start);
+                    positionToShow = start;
                 }catch(Exception e){
                     movesToShow = Collections.emptyList();
+                    positionToShow=null;
+                    System.out.println("Invalid position");
                 }
             }else if(line.startsWith("move")){
                 movesToShow = List.of();
 
-                var startPos=new ChessPosition(line.charAt(6)-48, line.charAt(5)-96);
-                var endPos=new ChessPosition(line.charAt(9)-48, line.charAt(8)-96);
+                ChessPosition startPos;
+                ChessPosition endPos;
+                try {
+                    startPos = new ChessPosition(line.charAt(6) - 48, line.charAt(5) - 96);
+                }catch(Exception e){ System.out.println("Invalid start position"); continue; }
+                try {
+                    endPos = new ChessPosition(line.charAt(9) - 48, line.charAt(8) - 96);
+                }catch(Exception e){ System.out.println("Invalid end position"); continue; }
 
-                var move = game.validMoves(startPos).stream().filter(maybeMove->
+                Collection<ChessMove> moves=game.validMoves(startPos);
+                if(moves==null){ System.out.println("No piece at this position"); continue; }
+                var move = moves.stream().filter(maybeMove ->
                         maybeMove.getEndPosition().equals(endPos)).findFirst();
-                if(move.isPresent()){
-                    try {
+
+                try{
+                    if (move.isPresent())
                         game.makeMove(move.get());
-                    }catch(Exception ignored){}
+                    else
+                        System.out.println("Illegal move");
+                }catch(InvalidMoveException e){
+                    System.out.println("Illegal move");
                 }
             }else{
                 System.out.println("command not recognized, type \"help\" to see all commands");
--- shared/src/main/java/chess/ChessRenderer.java
+++ shared/src/main/java/chess/ChessRenderer.java
@@ -6,12 +6,17 @@ import java.util.Map;
 public class ChessRenderer {
     public static class RenderData{
         private final Collection<ChessPiece.Offset> highlightedPositions;
-        private RenderData(Collection<ChessPiece.Offset> highlightedPositions){
+        private final ChessPiece.Offset originPosition;
+        private RenderData(Collection<ChessPiece.Offset> highlightedPositions,
+                           ChessPiece.Offset originPosition){
             this.highlightedPositions=highlightedPositions;
+            this.originPosition=originPosition;
         }
-        public static RenderData from(Collection<ChessMove> movesToShow){
+        public static RenderData from(Collection<ChessMove> movesToShow, ChessPosition positionToShow){
             return new RenderData(movesToShow.stream().map(move->new ChessPiece.Offset(move.getEndPosition().getColumn()-1,
-                    move.getEndPosition().getRow()-1)).toList());
+                    move.getEndPosition().getRow()-1)).toList(),
+                    positionToShow!=null?new ChessPiece.Offset(positionToShow.getColumn()-1, positionToShow.getRow()-1):
+                            new ChessPiece.Offset(-1,-1));
         }
     }
     private static final Map<ChessPiece.PieceType, String> blackPieces = Map.ofEntries(
@@ -95,15 +100,6 @@ public class ChessRenderer {
             this.fg=fg;
             this.bg=bg;
         }
-        static CLColor getOpposite(CLColor color){
-            return switch (color){
-                case WHITE -> BLACK;
-                case GRAY -> DARK_GRAY;
-                case DARK_GRAY -> GRAY;
-                case BLACK -> WHITE;
-                case CLEAR -> CLEAR;
-            };
-        }
         static CLColor getComplimentary(CLColor color){
             return switch (color){
                 case WHITE -> DARK_GRAY;
@@ -127,11 +123,11 @@ public class ChessRenderer {
     public static void renderGame(ChessGame game, RenderData data){
         for(int y=0;y<=8;y++){
             setColor(CLColor.CLEAR, CLColor.CLEAR);
-            System.out.println("   "+"+-------".repeat(8)+"+");
+            if(!canShowColor) System.out.println("   "+"+-------".repeat(8)+"+");
             if(y==8){
                 System.out.print("   ");
                 for(int x=0;x<8;x++){
-                    System.out.print("    "+(char)(x+97)+"   ");
+                    System.out.print((canShowColor?"   ":"    ")+(char)(x+97)+"   ");
                 }
                 System.out.print("\n\n   ");
                 setComplimentaryColor(game.getTeamTurn()== ChessGame.TeamColor.WHITE?CLColor.WHITE:CLColor.BLACK);
@@ -145,12 +141,13 @@ public class ChessRenderer {
                 for (int x = 0; x < 8; x++) {
                     var currPiece = game.getBoard().getPiece(new ChessPosition(8-y,x+1));
                     boolean isHighlighted = data.highlightedPositions.contains(new ChessPiece.Offset(x,7-y));
+                    boolean isOrigin = data.originPosition.equals(new ChessPiece.Offset(x,7-y));
                     if (x == 0) {
                         if(i==1) System.out.print(" "+(8-y)+" ");
                         else System.out.print("   ");
                     }
 
-                    System.out.print("|");
+                    if(!canShowColor) System.out.print("|");
 
                     setColor(currPiece==null?CLColor.CLEAR:
                                     (currPiece.getTeamColor()==ChessGame.TeamColor.WHITE?CLColor.WHITE:CLColor.GRAY),
@@ -163,7 +160,8 @@ public class ChessRenderer {
                     } else {
                         strToPrint="       ";
                     }
-                    if(isHighlighted){
+                    if(isHighlighted||isOrigin){
+                        var colors=isOrigin?new int[]{93,43}:new int[]{92,42};
                         var overlayLine = highlight.substring(i*8,i*8+7)+" ";
                         var chars = strToPrint.toCharArray();
                         strToPrint="";
@@ -171,9 +169,9 @@ public class ChessRenderer {
                             var overlayChar = overlayLine.charAt(j);
                             if(canShowColor) {
                                 if (overlayChar == '=' || overlayChar == '|') {
-                                    strToPrint+="\u001b[92;"+currColor[1]+"m";
+                                    strToPrint+="\u001b["+colors[0]+";"+currColor[1]+"m";
                                 } else if (overlayChar == '#') {
-                                    strToPrint+="\u001b[92;42m";
+                                    strToPrint+="\u001b["+colors[0]+";"+colors[1]+"m";
                                 } else {// space
                                     strToPrint+="\u001b["+currColor[0]+";"+currColor[1]+"m";
                                 }
@@ -185,7 +183,7 @@ public class ChessRenderer {
                     setColor(CLColor.CLEAR, CLColor.CLEAR);
 
                     if(x==7)
-                        System.out.println("|");
+                        System.out.println(canShowColor?"":"|");
                 }
             }
         }
